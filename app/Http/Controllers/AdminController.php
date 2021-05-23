a233fb75d7f0a6b169f0a8630e5f3d2481118a03
zeph1re <arlinnog1122@gmail.com>
2021-05-23 21:45:22 +0700

    admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin');
+        return view('admin.dashboard');
+    }
+
+    public function user()
+    {
+        $data_user = \App\User::all();
+        return view('admin.user', ['data_user' => $data_user]);
+    }
+
+    public function sports()
+    {
+        return view('admin.sports');
+    }
+
+    public function add()
+    {
+    }
+
+    public function delete()
+    {
+    }
+
+    public function edit()
+    {
     }
 }
